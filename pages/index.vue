174a22cde69186550d3e7fad8bd47a82b239b28b
RazinTeqB <razin@teqbuddies.com>
2023-02-07 10:18:10 +0530

    design

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="index text-danger">
-    dsfldj
+
+
     <SliderComponent/>
+    <SearchBar />
+
+    <div class="py-3">
+      <ProductBox  />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
 
-useHead({
-    script: 'bootstrap/dist/js/bootstrap.bundle.min.js'
-})
+// useHead({
+//     script: 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// })
 
 onMounted(() => {
   console.log("%cMounted", "color:#00ff00;font-size:20px;font-weight:bold;");
